def5fb0253b3a6dcc3d1c1cd898b2bda253a944d
johpetsc <johpetsc@gmail.com>
2021-11-18 02:41:54 -0300

    Updated README and new video files.

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# stereo-vision
+# Stereo Vision
+Measurements and tracking based on two cameras with different configurations. The program first calibrates both cameras based on pictures of a chess board, calculating the intrinsic matrix and the distortion. After that, based on recordings of two cameras capturing the same scene, the program:
+
+- generates the rotation matrix and translation vector
+- calculates both cameras position in a 3D space
+- executes the stereo calibration
+- rectifies and synchronizes the frames of the cameras
+- generates epipolar lines for the captured objects
+- computes the disparity and depth
+- tracks a moving object and calculates its position in a 3d space
+
+# Requirements
+```
+    $ pip install opencv-python
+```
+
+# Execute
+Camera intrinsic calibration:
+```
+    $ python src/stereo-vision.py --r1
+```
+Camera extrinsic calibration:
+```
+    $ python src/stereo-vision.py --r2
+```
+Cameras disparity and depth:
+```
+    $ python src/stereo-vision.py --r3
+```
+Object tracking:
+```
+    $ python src/stereo-vision.py --r4
+```
+1. Select an object to be tracked in both camera 1 and then press ENTER
+2. Select an object to be tracked in both camera 2 and then press ENTER
+3. Press Q and then hold W to track the object (release W to pause)
+4. Press Q twice to exit before the video finishes
--- src/stereo-vision.py
+++ src/stereo-vision.py
@@ -23,18 +23,18 @@ def findBoardInImage(images, objp, objpoints, imgpoints):
 
     return objpoints, imgpoints, img
 
-def camCalibration(folder):#Calibração de intrínsecos
+def camCalibration(folder): # intrinsic calibration
     objp = np.zeros((ROWS*COLUMNS, 3), np.float32)
     objp[:,:2] = np.mgrid[0:COLUMNS,0:ROWS].T.reshape(-1,2)
     objpoints = [] # 3d point in real world space
     imgpoints = [] # 2d points in image plane.
 
-    path = "data/images/" + folder + "/*.jpg"#Pega todas imagens da pasta
+    path = "data/images/" + folder + "/*.jpg" # reads every image in the folder
     images = glob.glob(path)
-    #Encontra as coordenadas do tabuleiro
+    # finds board coordinates
     objpoints, imgpoints, frame = findBoardInImage(images, objp, objpoints, imgpoints)
     cv.destroyAllWindows()
-    #Faz a calibração 
+    # calibrates the camera 
     ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, (frame.shape[1], frame.shape[0]),None,None)
     h, w = frame.shape[:2]
     newmtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 0, (w,h))
@@ -42,7 +42,7 @@ def camCalibration(folder):#Calibração de intrínsecos
     return mtx, dist, newmtx
 
 def extrinsicCalibration(mtx1, dist1, mtx2, dist2):
-    #Coordenadas 3D
+    # 3d space coordinates
     objp = np.zeros((8,3), np.float32)
     objp[0] = [0, 0, 0]
     objp[1] = [0, 1.40, 0]
@@ -61,7 +61,7 @@ def extrinsicCalibration(mtx1, dist1, mtx2, dist2):
                  [2.60, 0.70, 0],
                  [1.30, 0, 0],
                  [1.30, 1.40, 0]]
-    #Pontos 2D na câmera 1           
+    # 2d points in camera 1           
     imgp = np.zeros((8,2), np.float32)
     imgp[0] = [716, 60]
     imgp[1] = [1072, 110]
@@ -73,10 +73,10 @@ def extrinsicCalibration(mtx1, dist1, mtx2, dist2):
     imgp[7] = [1045, 261]
     imgpoints1 = [] # 3d point in real world space
     imgpoints1.append(imgp)
-    #Gera os valores extrínsecos da câmera 1
+    # generates the extrinsic values for camera 1
     retval1, rvec1, tvec1 = cv.solvePnP(np.float32(objpoints), np.float32(imgpoints1), mtx1, dist1)
     dst1, jacobian1 = cv.Rodrigues(rvec1)
-    #Pontos 2D na câmera 2
+    # 2d points in camera 2
     imgp2 = np.zeros((8,2), np.float32)
     imgp2[0] = [220, 50]
     imgp2[1] = [630, 34]
@@ -88,11 +88,11 @@ def extrinsicCalibration(mtx1, dist1, mtx2, dist2):
     imgp2[7] = [760, 170]
     imgpoints2 = [] # 3d point in real world space
     imgpoints2.append(imgp2)
-    #Gera os valores extrínsecos da câmera 2
+    # generates the extrinsic values for camera 2
     retval2, rvec2, tvec2 = cv.solvePnP(np.float32(objpoints), np.float32(imgpoints2), mtx2, dist2)
     dst2, jacobian2 = cv.Rodrigues(rvec2)
 
-    #Calcula a posição da câmera no espaço 3D
+    # calculates the camera position in the 3d space
     cameraPosition = -np.matrix(dst1).T * np.matrix(tvec1)
     #print(cameraPosition)
     cameraPosition = -np.matrix(dst2).T * np.matrix(tvec2)
@@ -103,10 +103,10 @@ def extrinsicCalibration(mtx1, dist1, mtx2, dist2):
     
     return objpoints, imgpoints1, imgpoints2, dst1, dst2, tvec1, tvec2, rvec1, rvec2
 
-def stereo():#Utilizada para pegar os frames sincronizados
-    camera1 = cv.VideoCapture('data/out/camera1/camera1_20fps.webm')
-    camera2 = cv.VideoCapture('data/out/camera2/camera2_20fps.webm')
-    for _ in range(102):#O vídeo da câmera 1 começa antes, ignora os frames adicionais
+def stereo(): # synchronizes different camera frames
+    camera1 = cv.VideoCapture('data/out/camera1/camera1_20fps.mp4')
+    camera2 = cv.VideoCapture('data/out/camera2/camera2_20fps.mp4')
+    for _ in range(102): # ignores initial camera 1 frames
         frame1 = camera1.read()[1]
     while(1):
         frame1 = camera1.read()[1]
@@ -122,7 +122,7 @@ def stereo():#Utilizada para pegar os frames sincronizados
             break
     return frame1, frame2
 
-def _3DProjection(rvec, tvec, mtx, dist, imgpoints, frame):#Gera os eixos X, Y e Z em algum frame da imagem
+def _3DProjection(rvec, tvec, mtx, dist, imgpoints, frame): # generates the X, Y and Z axis for an image frame
     axis = np.float32([[2.60,1.20,0], [2.10,0.70,0], [2.60,0.70,0.50]]).reshape(-1,3)
     proj, jac = cv.projectPoints(axis, rvec, tvec, mtx, dist)
     corner = tuple(imgpoints[0][5].ravel())
@@ -135,25 +135,20 @@ def _3DProjection(rvec, tvec, mtx, dist, imgpoints, frame):#Gera os eixos X, Y e
     cv.waitKey(888888)
     cv.destroyAllWindows()        
 
-def tracking(rmap_r_x, rmap_r_y, rmap_l_x, rmap_l_y):#Rastreamento de um objeto nas duas câmeras, as coordenadas 2D são armazenas para cada frame
-    camera1 = cv.VideoCapture('data/out/camera1/camera1_20fps.webm')
-    camera2 = cv.VideoCapture('data/out/camera2/camera2_20fps.webm')
-    points_r = []#Pontos 2D para cada frame da camera 1
-    points_l = []#Pontos 2D para cada frame da camera 2
-    for _ in range(150):#Pula os frames anteriores ao carrinho caindo no tabuleiro
+def tracking(rmap_r_x, rmap_r_y, rmap_l_x, rmap_l_y): # 3d object tracking in both cameras, storing the 2d coordinates for each frame
+    camera1 = cv.VideoCapture('data/out/camera1/camera1_20fps.mp4')
+    camera2 = cv.VideoCapture('data/out/camera2/camera2_20fps.mp4')
+    points_r = [] # 2d points for camera 1 frames
+    points_l = [] # 2d points for camera 2 frames
+    for _ in range(150): # skips the frames preceding the car hitting the board
         frame_r = camera1.read()[1]
     for _ in range(47):
        frame_l = camera2.read()[1]
 
     frame_r = cv.resize(frame_r, (1280, 720))
     frame_l = cv.resize(frame_l, (1280, 720))
-    #Feito o mapeamento para que as coordenadas sejam do frame retificado
     dst_r = cv.remap(frame_r, rmap_r_x, rmap_r_y, interpolation=cv.INTER_LINEAR)
     dst_l = cv.remap(frame_l, rmap_l_x, rmap_l_y, interpolation=cv.INTER_LINEAR)
-    #Tratamento das cores para que o rastreamento seja facilitado
-    #dst_r = cv.cvtColor(dst_r, cv.COLOR_BGR2LAB)
-    #dst_l = cv.cvtColor(dst_l, cv.COLOR_BGR2LAB)
-    #Tracker para as duas câmeras
     ROI_r = cv.selectROI("Tracking >", dst_r)
     tracker_r = cv.TrackerMIL_create()
     ROI_l = cv.selectROI("Tracking <", dst_l)
@@ -167,40 +162,35 @@ def tracking(rmap_r_x, rmap_r_y, rmap_l_x, rmap_l_y):#Rastreamento de um objeto 
     
 
     while camera1.isOpened():
-        pnt_r = np.zeros((1,2), np.float32)#Onde será armazenada a coordenada 2D do frame
+        pnt_r = np.zeros((1,2), np.float32)
         pnt_l = np.zeros((1,2), np.float32)
         frame_r = camera1.read()[1]
         frame_l = camera2.read()[1]
 
         frame_r = cv.resize(frame_r, (1280, 720))
         frame_l = cv.resize(frame_l, (1280, 720))
-        #Feito o mapeamento para que as coordenadas sejam do frame retificado
         dst_r = cv.remap(frame_r, rmap_r_x, rmap_r_y, interpolation=cv.INTER_LINEAR)
         dst_l = cv.remap(frame_l, rmap_l_x, rmap_l_y, interpolation=cv.INTER_LINEAR)
-        #Tratamento das cores para que o rastreamento seja facilitado
-        #dst_r = cv.cvtColor(dst_r, cv.COLOR_BGR2LAB)
-        #dst_l = cv.cvtColor(dst_l, cv.COLOR_BGR2LAB)
         pos_l = tracker_l.update(dst_l)
         pos_r = tracker_r.update(dst_r)
-        #Gera o retângulo que é mostrado na imagem
+        # generates the bbox
         p1_r = (int(pos_r[1][0]), int(pos_r[1][1]))
         p2_r = (int(pos_r[1][0] + pos_r[1][2]), int(pos_r[1][1] + pos_r[1][3]))
         cv.rectangle(dst_r, p1_r, p2_r, (255, 0, 0))
         p1_l = (int(pos_l[1][0]), int(pos_l[1][1]))
         p2_l = (int(pos_l[1][0] + pos_l[1][2]), int(pos_l[1][1] + pos_l[1][3]))
         cv.rectangle(dst_l, p1_l, p2_l, (255, 0, 0))
-        #Pega a coordenada 2D
+        # 2d coordinate
         pnt_r[0] = [int(pos_r[1][0]+(pos_r[1][2]/2)), int(pos_r[1][1]+(pos_r[1][3]/2))]
         pnt_l[0] = [int(pos_l[1][0]+(pos_l[1][2]/2)), int(pos_l[1][1]+(pos_l[1][3]/2))]
-        #Insere a coordenada 2D na lista de coordenadas
         points_r.append(pnt_r)
         points_l.append(pnt_l)
 
         frame = np.hstack((dst_l, dst_r))
         cv.imshow('Tracking', frame)
-        if cv.waitKey(50000) & 0xFF == ord('q'):#Q->Q: break, Q->W: Pausa o rastreamento
-            if cv.waitKey(50000) & 0xFF == ord('w'):#Utilizar para ignorar frames no rastreamento(quando o topo sair do frame)
-                while(1):#Passa de frame sem rastrear até que seja pressionado Q
+        if cv.waitKey(50000) & 0xFF == ord('q'): # Q->Q: break, Q->W: pause tracking
+            if cv.waitKey(50000) & 0xFF == ord('w'): # ignores the frames where te object leaves the frame
+                while(1): 
                     frame_r = camera1.read()[1]
                     frame_l = camera2.read()[1]
                     frame_r = cv.resize(frame_r, (1280, 720))
@@ -221,73 +211,62 @@ def tracking(rmap_r_x, rmap_r_y, rmap_l_x, rmap_l_y):#Rastreamento de um objeto 
 def stereo_vision(req):
     print('...')
     size = (WIDTH, HEIGHT)
-    #Requisito 1
     mtx_r, dist_r, newmtx_r = camCalibration('camera1')
     mtx_l, dist_l, newmtx_l = camCalibration('camera2')
 
     if req == 1:
         print("CAMERA 1")
-        print("Matriz de intrínsecos:")
+        print("Intrinsic Matrix:")
         print(mtx_r)
-        print("Distorção:")
+        print("Distortion:")
         print(dist_r)
         print("CAMERA 2")
-        print("Matriz de insrínsecos:")
+        print("Intrinsic Matrix:")
         print(mtx_l)
-        print("Distorção:")
+        print("Distortion:")
         print(dist_l)
 
-    #Calibracao de instrinsecos pronta
-
-    #Requisito 2
     objpoints, imgpoints_r, imgpoints_l, dst_r, dst_l, tvec_r, tvec_l, rvec_r, rvec_l = extrinsicCalibration(mtx_r, dist_r, mtx_l, dist_l)
     
     if req == 2:
-        print("Matriz de rotação:")
+        print("Rotation Matrix:")
         print(dst_r)
-        print("Vetor de translação:")
+        print("Translation Vector:")
         print(tvec_r)
-        print("Posição da câmera:")
+        print("Camera position:")
         print(np.transpose(dst_r))
         print(-np.transpose(dst_r))
         X, Y, Z = np.dot(-np.transpose(dst_r), tvec_r)
         print("X:", X, "Y:", Y, "Z:", Z)
-        print("Matriz de rotação:")
+        print("Rotation Matrix:")
         print(dst_l)
-        print("Vetor de translação:")
+        print("Translation Vector:")
         print(tvec_l)
-        print("Posição da câmera:")
+        print("Camera position:")
         X, Y, Z = np.dot(-np.transpose(dst_l), tvec_l)
         print("X:", X, "Y:", Y, "Z:", Z)
 
-    #Calibracao de extrinsecos pronta
-
-    #Requisito 3
     if req > 2:
-        #Realiza a calibração estéreo
+        # stereo calibration
         retval, cameraMatrix_l, distCoeffs_l, cameraMatrix_r, distCoeffs_r, R, T, E, F = cv.stereoCalibrate(objpoints, imgpoints_l, imgpoints_r, 
                                                                                                         mtx_l, dist_l, mtx_r, 
                                                                                                         dist_r, tuple(size))
 
-        #Calcula a retificação de uma câmera em relação à outra
         R_l, R_r, P_l, P_r, Q, validPixROI1, validPixROI2 = cv.stereoRectify(cameraMatrix_l, distCoeffs_l, cameraMatrix_r, distCoeffs_r, tuple(size), R, T, alpha=0.92, newImageSize=tuple(size))
         
-        #Calcula o mapeamento para a imagem retificada
         rmap_r_x, rmap_r_y = cv.initUndistortRectifyMap(mtx_r, dist_r, R_r, P_r, tuple(size), cv.CV_32F)
         rmap_l_x, rmap_l_y = cv.initUndistortRectifyMap(mtx_l, dist_l, R_l, P_l, tuple(size), cv.CV_32F)
     
     if req == 3:
         frame_r, frame_l = stereo()
-        #Funções que geram uma projeção dos eixos em cada frame
         #_3DProjection(rvec_r, tvec_r, mtx_r, dist_r, imgpoints_r, frame_r)
         #_3DProjection(rvec_l, tvec_l, mtx_l, dist_l, imgpoints_l, frame_l)
         cv.destroyAllWindows()
-        #Remapeando os frames para ficarem retificados
         dst_r = cv.remap(frame_r, rmap_r_x, rmap_r_y, interpolation=cv.INTER_LINEAR)
         dst_l = cv.remap(frame_l, rmap_l_x, rmap_l_y, interpolation=cv.INTER_LINEAR)
 
         frame = np.hstack((dst_l, dst_r))
-        #Desenha as linhas epipolares. As linhas são retas e não precisam ser calculadas porque as imagens foram retificadas
+        # epipolar lines
         frame = cv.line(frame, (0, 505), (2560,505), (255, 0, 0), 1)
         frame = cv.line(frame, (0, 480), (2560,480), (0, 255, 0), 1)
         frame = cv.line(frame, (0, 160), (2560,160), (0, 0, 255), 1)
@@ -304,15 +283,14 @@ def stereo_vision(req):
                                     P1=50, P2=100, disp12MaxDiff=10, uniquenessRatio=12,
                                     speckleWindowSize=0, speckleRange=2, preFilterCap=100)
         
-        disparity = ster.compute(frame2_gray, frame1_gray)#Computa disparidade
-        image3D = cv.reprojectImageTo3D(disparity, Q)#Gera uma matrix x, y, 3, onde para cada ponto x, y tem uma coordenada 3D
-        depth = image3D[:,:,1]#Pegando coordenada de profundidade
+        disparity = ster.compute(frame2_gray, frame1_gray) # computes the disparity
+        image3D = cv.reprojectImageTo3D(disparity, Q) 
+        depth = image3D[:,:,1] # depth coordinates
         norm = cv.normalize(disparity, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F)
         depth = cv.normalize(depth, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F)
 
-        import matplotlib.pyplot as plt#Import feito aqui porque o backend do matplotlib e opencv estavam tendo conflito
+        import matplotlib.pyplot as plt
         from mpl_toolkits.axes_grid1 import make_axes_locatable
-        #Criar imagem de disparidade e profundiade com colorbar
         fig, ax = plt.subplots()
         fig2, ax2 = plt.subplots()
         divider = make_axes_locatable(ax)
@@ -322,21 +300,20 @@ def stereo_vision(req):
         im = ax.imshow(norm, 'gray')
         im2 = ax2.imshow(depth, 'gray')
         fig.colorbar(im, cax=cax, orientation='vertical')
-        fig.suptitle("Disparidade")
+        fig.suptitle("Disparity")
         fig2.colorbar(im2, cax=cax2, orientation='vertical')
-        fig2.suptitle("Profundidade")
+        fig2.suptitle("Depth")
         plt.show()
     
-    if req == 4:#Chama a função de rastreamento e gera as coordenadas em 3D usando a triangulação de pontos
+    if req == 4:
         points_l, points_r = tracking(rmap_r_x, rmap_r_y, rmap_l_x, rmap_l_y)
         points = cv.triangulatePoints(P_l, P_r, np.float32(points_l), np.float32(points_r))
         X, Y, Z = points[0]/points[3], points[1]/points[3], points[2]/points[3]# X: x/w, Y: y/w, Z: z/w
 
-        import matplotlib.pyplot as plt#Import feito aqui porque o backend do matplotlib e opencv estavam tendo conflito
-        from mpl_toolkits.axes_grid1 import make_axes_locatable
-        #Gráficos representando a posição do objeto rastreado em relação à câmera 1
+        import matplotlib.pyplot as plt
+        from mpl_toolkits.axes_grid1 import make_axes_locatable1
         fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
-        fig.suptitle("Variação do objeto para os eixos X, Y, Z")
+        fig.suptitle("Object variation for axis X, Y, Z")
 
         ax1.plot(X)
         ax1.set_ylabel('X')
@@ -351,12 +328,12 @@ def stereo_vision(req):
         ax3.set_ylim([-4, -1])
         plt.show()
 
-def camera1_fps(folder):#Cria um novo arquivo de vídeo para a câmera 1, pulando um frame a cada 16 para encurtar o total de frames
+def camera1_fps(folder): # creates a new video file for camera 1 to standardize its fps
     video = cv.VideoCapture("data/" + folder + ".webm")
-    fourcc = cv.VideoWriter_fourcc(*'VP80')
+    fourcc = cv.VideoWriter_fourcc(*'mp4v')
     skip = 0
     frames = 16
-    writer = cv.VideoWriter('data/out/' + folder + '/' + folder + '_20fps.webm', fourcc, 20.0, (1920, 1080))
+    writer = cv.VideoWriter('data/out/' + folder + '/' + folder + '_20fps.mp4', fourcc, 20.0, (1920, 1080))
     while video.isOpened():
         ret, frame = video.read()
         skip += 1
@@ -369,10 +346,10 @@ def camera1_fps(folder):#Cria um novo arquivo de vídeo para a câmera 1, puland
     writer.release()
     video.release()
 
-def camera2_fps(folder):#Cria um novo arquivo de vídeo para a câmera 2, salvando cada frame duas vezes para dobrar o total de frames
+def camera2_fps(folder): # creates a new video file for camera 2 to standardize its fps
     video = cv.VideoCapture("data/" + folder + ".webm")
-    fourcc = cv.VideoWriter_fourcc(*'VP80')
-    writer = cv.VideoWriter('data/out/' + folder + '/' + folder + '_20fps.webm', fourcc, 20.0, (1280, 720))
+    fourcc = cv.VideoWriter_fourcc(*'mp4v')
+    writer = cv.VideoWriter('data/out/' + folder + '/' + folder + '_20fps.mp4', fourcc, 20.0, (1280, 720))
     while video.isOpened():
         ret, frame = video.read()
         if ret == False:
@@ -382,21 +359,17 @@ def camera2_fps(folder):#Cria um novo arquivo de vídeo para a câmera 2, salvan
     writer.release()
     video.release()
 
-def main():#Requisitos
+def main():
     if sys.argv[1] == '--r1':
-        #Requisito 1
         stereo_vision(1)
     elif sys.argv[1] == '--r2':
-        #Requisito 2
         stereo_vision(2)
     elif sys.argv[1] == '--r3':
-        #Requisito 3
         stereo_vision(3)
     elif sys.argv[1] == '--r4':
-        #Requisito 4
         stereo_vision(4)
-    else:#Esse passo é necessário para o código rodar corretamente
-         #Serão criados novos arquivos de vídeo com o mesmo FPS
+    else: # this is needed for the code to work properly
+          # new video files with 20 fps will be creates for both camera 1 and 2
         #22(ish)fps -> 20 fps
         camera1_fps('camera1')
         #10fps -> 20 fps
